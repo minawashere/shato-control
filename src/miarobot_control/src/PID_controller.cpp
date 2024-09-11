243c64037bb46bb3e855ce481df6401a37b1cbaa
minawashere <minasalama2580@gmail.com>
2024-09-11 13:43:18 +0000

    added integral and differential functions

--- src/miarobot_control/src/PID_controller.cpp
+++ src/miarobot_control/src/PID_controller.cpp
@@ -13,13 +13,19 @@
 #include "tf2/LinearMath/Matrix3x3.h"
 #include "tf2/LinearMath/Quaternion.h"
 
+namespace gm = geometry_msgs;
+namespace stdm = std_msgs;
+namespace nav = nav_msgs;
+
+
 const int max_x = 2;
 const int max_y = 2;
 const int max_r = 2;
 
+
 float get_time(){
     ros::Time time = ros::Time::now();
-    return time.toSec();
+    return (float)time.toSec();
 }
 
 struct AxisRot {
@@ -32,19 +38,20 @@ struct AxisRot {
     }
 };
 
-class FetchData {
+struct FetchData {
     tf2::Quaternion q;
-    geometry_msgs::Point p;
-    geometry_msgs::Vector3 linear;
-    geometry_msgs::Vector3 angular;
+    gm::Point p;
+    gm::Vector3 linear;
+    gm::Vector3 angular;
+    
 
-public:
+
     tf2::Quaternion get_q() {return q;}
-    geometry_msgs::Point get_p(){return p;}
-    geometry_msgs::Vector3 get_linear(){return linear;}
-    geometry_msgs::Vector3 get_angular(){return angular;}
+    gm::Point get_p(){return p;}
+    gm::Vector3 get_linear(){return linear;}
+    gm::Vector3 get_angular(){return angular;}
 
-    void fetch_data(const nav_msgs::Odometry::ConstPtr& msg) {
+    void fetch_data(const nav::Odometry::ConstPtr& msg) {
         q.setX(msg->pose.pose.orientation.x);
         q.setY(msg->pose.pose.orientation.y);
         q.setZ(msg->pose.pose.orientation.z);
@@ -65,49 +72,78 @@ public:
 
 };
 
-struct SetPoint{
-    float set_x, set_y, set_theta;
-    void FetchSetPoint(const geometry_msgs::Pose2D::ConstPtr& msg){
-        set_x = msg->x;
-        set_y = msg->y;
-        set_theta = msg->theta;
+struct Point{
+    float x, y, theta;
+    // Pid x_pid, y_pid, theta_pid;
+
+    void FetchPoint(const gm::Pose2D::ConstPtr& msg){
+        x = msg->x;
+        y = msg->y;
+        theta = msg->theta;
     }
 };
 
-struct PID_param {
+struct PidParam{
+
     float kp, ki, kd;
-    void FetchParam(const geometry_msgs::Vector3::ConstPtr& msg){
+
+    void FetchParam(const gm::Vector3::ConstPtr& msg){
         kp = msg->x;
         ki = msg->y;
         kd = msg->z;
     }
+} param;
+
+struct Pid { 
+private:
+    float accum = 0;
+    float prev_time_i = 0;
+    float prev_time_d = 0;
+    float prev_point = 0;
+
+    void integrate(const float current){
+        float curr_time = get_time();
+        this->accum += current*(curr_time - prev_time_i);
+        prev_time_i = curr_time;
+    }
+    float differentiate(const float current,const float last){
+        float curr_time = get_time();
+        float d = (float)((current - last)/(curr_time - prev_time_d));
+        prev_time_d = curr_time;
+        return d;
+    }
+public:
+ 
+
+
 };
 
-struct PID{};
+void publish_output(gm::Twist msg, ros::Publisher pub){
+    pub.publish(msg);
+}
 
 int main(int argc, char **argv) {
     FetchData data;
-    SetPoint setPoint;
-    PID_param param;
-
+    Point setPoint;
+    // Pid 
     //x
     //y
     //theta
-    
+
     
     
     ros::init(argc, argv, "PID");
 
     ros::NodeHandle nh;
     ros::Subscriber sub = nh.subscribe("/odom", 1000, &FetchData::fetch_data, &data);
-    ros::Subscriber sub2 = nh.subscribe("/setpoint", 100, &SetPoint::FetchSetPoint, &setPoint);
-    ros::Subscriber sub3 = nh.subscribe("/set_param", 100, &PID_param::FetchParam, &param);
-    ros::Publisher pub = nh.advertise<std_msgs::Float64MultiArray>("/cmd_vel", 1000);
+    ros::Subscriber sub2 = nh.subscribe("/setpoint", 100, &Point::FetchPoint, &setPoint);
+    ros::Subscriber sub3 = nh.subscribe("/set_param", 100, &PidParam::FetchParam, &param);
+    ros::Publisher pub = nh.advertise<gm::Twist>("/cmd_vel", 1000);
 
     ros::Rate loop_rate(800);
 
     while (ros::ok()) {
-        // publish_rot(AxisRot(fq.get()), pub);
+        publish_output(AxisRot(f.get()), pub);
         ros::spinOnce();
         loop_rate.sleep();
     }
