8f5c3eda4c24c9447291f1fdf2dc64f0b315da73
minawashere <minasalama2580@gmail.com>
2024-09-12 04:05:05 +0000

    Header file

--- src/miarobot_control/src/PID_controller.cpp
+++ src/miarobot_control/src/PID_controller.cpp
@@ -1,19 +1,4 @@
-#include "ros/ros.h"
-
-#include "std_msgs/Float64MultiArray.h"
-
-#include "geometry_msgs/Pose.h"
-#include "geometry_msgs/Pose2D.h"
-#include "geometry_msgs/Point.h"
-#include "geometry_msgs/Twist.h"
-#include "geometry_msgs/Vector3.h"
-
-#include "nav_msgs/Odometry.h"
-#include "tf2/LinearMath/Matrix3x3.h"
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2/LinearMath/Transform.h"
-
-#include <chrono>
+#include <PID.h>
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #define max(a, b) (((a) > (b)) ? (a) : (b))
